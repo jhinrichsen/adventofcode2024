83747a00a7c1f673f4a7a518e598073c7dfdf9e9
Jochen Hinrichsen <jochen.hinrichsen@kambrium.net>
2024-12-01 23:36:08 +0100

    perf(day01): custom parser

--- day01.go
+++ day01.go
@@ -2,49 +2,70 @@ package adventofcode2023
 
 import (
 	"slices"
-	"strconv"
-	"strings"
 )
 
-func Day01(lines []string, part1 bool) uint {
-	atoi := func(s string) (int, int) {
-		parts := strings.Fields(s)
-		l, _ := strconv.Atoi(parts[0])
-		r, _ := strconv.Atoi(parts[1])
-		return l, r
-	}
-
-	left, right := make([]int, len(lines)), make([]int, len(lines))
-	for i := range lines {
-		left[i], right[i] = atoi(lines[i])
-	}
+func Day01(buf []byte, part1 bool) uint {
+	list := twoUints(buf)
 
 	var sum uint
 	if part1 {
-		slices.Sort(left)
-		slices.Sort(right)
+		slices.Sort(list[0])
+		slices.Sort(list[1])
 
-		delta := func(x, y int) uint {
+		delta := func(x, y uint) uint {
 			return uint(max(x, y) - min(x, y))
 		}
 
-		for i := range lines {
-			sum += delta(left[i], right[i])
+		for i := range list[0] {
+			sum += delta(list[0][i], list[1][i])
 		}
 	} else {
-
-		histogram := make(map[int]uint, len(lines)) // len(lines) is the worst case without any  duplicates
-		for i := range right {
-			histogram[right[i]] += 1
+		histogram := make(map[uint]uint, len(list[1])) // len() is the worst case without any  duplicates
+		for i := range list[1] {
+			histogram[list[1][i]] += 1
 		}
 
-		for i := range left {
-			sum += uint(left[i]) * histogram[left[i]]
+		for i := range list[0] {
+			sum += uint(list[0][i]) * histogram[list[0][i]]
 		}
 	}
 	return sum
 }
 
-func twoUint(buf []byte) [][2]uint {
-	return nil
+func twoUints(buf []byte) [2][]uint {
+	var dim uint
+	for i := range buf {
+		if buf[i] == '\n' {
+			dim++
+		}
+	}
+	is := [...][]uint{
+		make([]uint, dim),
+		make([]uint, dim),
+	}
+
+	for i, idx := 0, 0; i < len(buf); i++ {
+		var n uint
+
+		// left number
+		for buf[i] != ' ' {
+			n = 10*n + uint(buf[i]-'0')
+			i++
+		}
+		is[0][idx] = n
+
+		// 3 spaces as field separator
+		i += 3
+
+		// right number
+		n = 0
+		for buf[i] != '\n' {
+			n = 10*n + uint(buf[i]-'0')
+			i++
+		}
+		is[1][idx] = n
+		idx++
+	}
+
+	return is
 }
--- day01_test.go
+++ day01_test.go
@@ -1,16 +1,18 @@
 package adventofcode2023
 
 import (
+	"os"
+	"slices"
 	"testing"
 )
 
 func TestDay01Part1Example(t *testing.T) {
 	const want = 11
-	lines, err := linesFromFilename(exampleFilename(01))
+	in, err := os.ReadFile(exampleFilename(01))
 	if err != nil {
 		t.Fatal(err)
 	}
-	got := Day01(lines, true)
+	got := Day01(in, true)
 	if want != got {
 		t.Fatalf("want %d but got %d", want, got)
 	}
@@ -18,34 +20,34 @@ func TestDay01Part1Example(t *testing.T) {
 
 func TestDay01Part1(t *testing.T) {
 	const want = 2166959
-	lines, err := linesFromFilename(filename(01))
+	in, err := os.ReadFile(filename(01))
 	if err != nil {
 		t.Fatal(err)
 	}
-	got := Day01(lines, true)
+	got := Day01(in, true)
 	if want != got {
 		t.Fatalf("want %d but got %d", want, got)
 	}
 }
 
 func BenchmarkDay01Part1(b *testing.B) {
-	lines, err := linesFromFilename(filename(01))
+	in, err := os.ReadFile(filename(01))
 	if err != nil {
 		b.Fatal(err)
 	}
 	b.ResetTimer()
 	for range b.N {
-		_ = Day01(lines, true)
+		_ = Day01(in, true)
 	}
 }
 
 func TestDay01Part2Example(t *testing.T) {
 	const want = 31
-	lines, err := linesFromFilename(exampleFilename(01))
+	in, err := os.ReadFile(exampleFilename(01))
 	if err != nil {
 		t.Fatal(err)
 	}
-	got := Day01(lines, false)
+	got := Day01(in, false)
 	if want != got {
 		t.Fatalf("want %d but got %d", want, got)
 	}
@@ -53,23 +55,47 @@ func TestDay01Part2Example(t *testing.T) {
 
 func TestDay01Part2(t *testing.T) {
 	const want = 23741109
-	lines, err := linesFromFilename(filename(01))
+	in, err := os.ReadFile(filename(01))
 	if err != nil {
 		t.Fatal(err)
 	}
-	got := Day01(lines, false)
+	got := Day01(in, false)
 	if want != got {
 		t.Fatalf("want %d but got %d", want, got)
 	}
 }
 
 func BenchmarkDay01Part2(b *testing.B) {
-	lines, err := linesFromFilename(filename(01))
+	in, err := os.ReadFile(filename(01))
 	if err != nil {
 		b.Fatal(err)
 	}
 	b.ResetTimer()
 	for range b.N {
-		_ = Day01(lines, false)
+		_ = Day01(in, false)
+	}
+}
+
+func TestTwoUint(t *testing.T) {
+	buf := []byte("1234   4321\n6789   9876\n")
+	left := []uint{1234, 6789}
+	right := []uint{4321, 9876}
+	got := twoUints(buf)
+	if !slices.Equal(left, got[0]) {
+		t.Fatalf("want left=%+v but got %+v", left, got[0])
+	}
+	if !slices.Equal(right, got[1]) {
+		t.Fatalf("want right=%+v but got %+v", right, got[1])
+	}
+}
+
+func BenchmarkTwoUint(b *testing.B) {
+	buf, err := os.ReadFile(filename(01))
+	if err != nil {
+		b.Fatal(err)
+	}
+	b.ResetTimer()
+	for range b.N {
+		_ = twoUints(buf)
 	}
 }
