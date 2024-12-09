da5b191718484b616d130c522864436a2c315206
Jochen Hinrichsen <jochen.hinrichsen@kambrium.net>
2024-12-10 00:16:24 +0100

    feat(day05): add part 2

--- day05.go
+++ day05.go
@@ -1,16 +1,17 @@
 package adventofcode2024
 
+import (
+	"slices"
+)
+
 type tuple struct {
 	before, after uint8
 }
 
-func Day05(lines []string) (sum uint) {
-	const N = 1200
-
+func Day05(lines []string, part1 bool) (sum uint) {
 	var (
-		nRules  int
-		rules   [N]tuple
-		numbers [N]uint8
+		nRules int
+		rules  [1176]tuple
 
 		digit = func(b byte) uint8 {
 			return b - '0'
@@ -37,30 +38,60 @@ func Day05(lines []string) (sum uint) {
 	// section 2: order
 
 	for _, line := range lines[nRules+1:] {
+		var (
+			numbers    [23]uint8
+			nNumbers   = (len(line) + 1) / 3
+			sortRules  [253]tuple
+			nSortRules = 0
+			ordered    = true
+		)
 
 		// parse section 2: numbers
-		n := (len(line) + 1) / 3
-		for j, idx := 0, 0; j < n; j++ {
+		for j, idx := 0, 0; j < nNumbers; j++ {
 			numbers[j] = ten(line[idx]) + digit(line[idx+1])
 			idx += 3
 		}
 
-		ordered := true
-		for j := range numbers[:n] {
-			for k := j + 1; k < n; k++ {
+		for j := range numbers[:nNumbers] {
+			for k := j + 1; k < nNumbers; k++ {
 				current := numbers[j]
 				succ := numbers[k]
 				for _, rule := range rules {
-					if current == rule.after && succ == rule.before {
+					bad := current == rule.after && succ == rule.before
+					if bad {
 						ordered = false
-						goto l1
+						if part1 { // done
+							goto l1
+						}
+					}
+
+					// for part 2, harvest all matching rules
+					good := current == rule.before && succ == rule.after
+					if good || bad {
+						sortRules[nSortRules] = rule
+						nSortRules++
 					}
 				}
 			}
 		}
 	l1:
-		if ordered {
-			middle := numbers[n/2]
+		if part1 && ordered {
+			middle := numbers[nNumbers/2]
+			sum += uint(middle)
+		} else if !part1 && !ordered {
+			slices.SortFunc(numbers[:nNumbers], func(x, y uint8) int {
+				for _, rule := range sortRules {
+					// ascending or descending does not really matter, because we pick the middle element at the end
+					if x == rule.before && y == rule.after {
+						return -1
+					}
+					if x == rule.after && y == rule.before {
+						return 1
+					}
+				}
+				return 0
+			})
+			middle := numbers[nNumbers/2]
 			sum += uint(middle)
 		}
 	}
--- day05_test.go
+++ day05_test.go
@@ -10,19 +10,19 @@ func TestDay05Part1Example(t *testing.T) {
 	if err != nil {
 		t.Fatal(err)
 	}
-	got := Day05(lines)
+	got := Day05(lines, true)
 	if want != got {
 		t.Fatalf("want %d but got %d", want, got)
 	}
 }
 
 func TestDay05Part2Example(t *testing.T) {
-	const want = 0
+	const want = 123
 	lines, err := linesFromFilename(exampleFilename(05))
 	if err != nil {
 		t.Fatal(err)
 	}
-	got := Day05(lines)
+	got := Day05(lines, false)
 	if want != got {
 		t.Fatalf("want %d but got %d", want, got)
 	}
@@ -34,19 +34,19 @@ func TestDay05Part1(t *testing.T) {
 	if err != nil {
 		t.Fatal(err)
 	}
-	got := Day05(lines)
+	got := Day05(lines, true)
 	if want != got {
 		t.Fatalf("want %d but got %d", want, got)
 	}
 }
 
 func TestDay05Part2(t *testing.T) {
-	const want = 0
+	const want = 7380
 	lines, err := linesFromFilename(filename(05))
 	if err != nil {
 		t.Fatal(err)
 	}
-	got := Day05(lines)
+	got := Day05(lines, false)
 	if want != got {
 		t.Fatalf("want %d but got %d", want, got)
 	}
@@ -59,7 +59,7 @@ func BenchmarkDay05Part1(b *testing.B) {
 	}
 	b.ResetTimer()
 	for range b.N {
-		_ = Day05(lines)
+		_ = Day05(lines, true)
 	}
 }
 
@@ -70,6 +70,6 @@ func BenchmarkDay05Part2(b *testing.B) {
 	}
 	b.ResetTimer()
 	for range b.N {
-		_ = Day05(lines)
+		_ = Day05(lines, false)
 	}
 }
