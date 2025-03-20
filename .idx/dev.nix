587cfbadbedf1e39532e8c9e8edb6dc87020f069
jochen.hinrichsen <jochen.hinrichsen@kambrium.net>
2025-03-20 10:51:47 +0000

    chore: add Asciidoc and Go support

--- .idx/dev.nix
+++ .idx/dev.nix
@@ -18,8 +18,9 @@
   idx = {
     # Search for the extensions you want on https://open-vsx.org/ and use "publisher.id"
     extensions = [
-      "vscodevim.vim"
+      "asciidoctor.asciidoctor-vscode"
       "golang.go"
+      "vscodevim.vim"
     ];
 
     # Enable previews
